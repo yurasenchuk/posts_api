b93cb01190a46d0f5e03b8249c48fd1964523e8d
yurasenchuk <senchukjuriy@gmail.com>
2021-02-18 18:39:48 +0200

    functional updated/refactored

--- backend/post/views.py
+++ backend/post/views.py
@@ -18,21 +18,9 @@ class PostCreateView(CreateAPIView):
     """
     This view created to have an opportunity to CREATE Post
     """
-
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (IsAuthenticated,)
-
-    # def get_serializer(self, *args, **kwargs):
-    #     """
-    #     This method overridden to have an opportunity to add current user as an author of new Post
-    #     """
-    #     serializer_class = self.get_serializer_class()
-    #     kwargs["context"] = self.get_serializer_context()
-    #     data = self.request.data.copy()
-    #     data["author"] = self.request.user.id
-    #     kwargs["data"] = data
-    #     return serializer_class(*args, **kwargs)
 
 
 class PostListView(ListAPIView):
